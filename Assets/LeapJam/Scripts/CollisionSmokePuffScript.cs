d17bf6d80c952b03d5af19d2232ef45118ba6603
johnjoemcbob <mcormack1994@hotmail.co.uk>
2015-10-25 21:40:28 +0000

    Minor Changes, Particles

---
+++ Assets/LeapJam/Scripts/CollisionSmokePuffScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionSmokePuffScript : MonoBehaviour
+{
+	public GameObject ParticlePrefab;
+	public float BetweenParticleFire = 2;
+
+	private float NextParticleFire = 0;
+
+    void OnCollisionEnter( Collision collision )
+	{
+		if ( NextParticleFire > Time.time ) return;
+
+        foreach ( ContactPoint contact in collision.contacts )
+		{
+			GameObject particle = (GameObject) Instantiate( ParticlePrefab );
+			particle.transform.position = contact.point;
+			//particle.transform.LookAt( contact.normal );
+
+			NextParticleFire = Time.time + BetweenParticleFire;
+			break;
+		}
+	}
+}
