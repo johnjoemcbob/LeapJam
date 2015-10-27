b738d4ded1af944a506ac87880321c0311c99f03
johnjoemcbob <mcormack1994@hotmail.co.uk>
2015-10-27 15:58:03 +0000

    Win Screen Initial Work

---
+++ Assets/HandRecordingPedestalScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandRecordingPedestalScript : MonoBehaviour
+{
+	void Update()
+	{
+		GetComponent<HandController>().PauseRecording();
+	}
+}
